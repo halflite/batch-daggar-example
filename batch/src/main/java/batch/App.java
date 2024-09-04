93681c1b6aa16a5cdd69718923cae7537fa695d1
halflite <shingo@halflite.net>
2024-09-04 22:46:16 +0900

    ログ出力と基本的なライブラリ追加

--- batch/src/main/java/batch/App.java
+++ batch/src/main/java/batch/App.java
@@ -1,13 +1,14 @@
 package batch;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/** Main App */
+public class App {
+  /** Logger */
+  private static Logger LOG = LoggerFactory.getLogger(App.class);
+
+  public static void main(String[] args) {
+    LOG.info("started.");
+  }
 }
